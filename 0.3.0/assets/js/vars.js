5b960514c2cc2043a3be0d0cfcca682524339df8
Niv Kaufman <nivkman@gmail.com>
2022-09-03 03:43:04 +0300

    return db vars

--- 0.3.0/assets/js/vars.js
+++ 0.3.0/assets/js/vars.js
@@ -8,20 +8,35 @@ const form = document.getElementById('varsForm');
 const msg = document.getElementById('message');
 const resetBtn = document.getElementById('reset');
 
+let vars = null;
+let u_i_global = "";
+let api_key = "";
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const vars = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        company: company.value,
-        title: title.value,
-        email: email.value,
-        phone: phone.value
+    const data = {
+        vars: {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            company: company.value,
+            title: title.value,
+            email: email.value,
+            phone: phone.value
+        }
     };
 
-    const vars_serialized = JSON.stringify(vars);
-    localStorage.setItem("vars", vars_serialized);
+    await fetch(`${API}/api/v1/profiles/vars?uid=${u_i_global}&k=${api_key}`, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
 
     msg.style.display = "block";
 });
@@ -36,18 +51,25 @@ resetBtn.addEventListener('click', (e) => {
 })
 
 const renderVars = async () => {
-    const vars = await getUserVars();
+    fetch(`${API}/api/v1/profiles/vars?uid=${u_i_global}&k=${api_key}`).then(r => r.json()).then(vars => {
+        firstName.value = vars.firstName ? vars.firstName : null;
+        lastName.value = vars.lastName ? vars.lastName : null;
+        company.value = vars.company ? vars.company : null;
+        title.value = vars.title ? vars.title : null;
+        email.value = vars.email ? vars.email : null;
+        phone.value = vars.phone ? vars.phone : null;
+    })
 
-    firstName.value = vars.firstName ? vars.firstName : null;
-    lastName.value = vars.lastName ? vars.lastName : null;
-    company.value = vars.company ? vars.company : null;
-    title.value = vars.title ? vars.title : null;
-    email.value = vars.email ? vars.email : null;
-    phone.value = vars.phone ? vars.phone : null;
 }
 
 const main = async () => {
-    await renderVars();
+    await chrome.cookies.getAll({ domain: `.${domain}` }, async (cookies) => {
+        if (cookies.length > 1) {
+            u_i_global = await cookies.find(elm => elm.name == 'u_i').value;
+            api_key = await cookies.find(elm => elm.name == 'k').value;
+            await renderVars();
+        }
+    })
 }
 
 main();
